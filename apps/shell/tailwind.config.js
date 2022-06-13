2403fc363584aad54635153bd887974ffc107f81
franky <franky@biteinvestments.com>
2022-06-13 18:46:25 +0800

    🔨 run nx migrate

--- apps/shell/tailwind.config.js
+++ apps/shell/tailwind.config.js
@@ -1,6 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 const { join } = require('path');
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
