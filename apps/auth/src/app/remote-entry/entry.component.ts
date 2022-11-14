e0fa8eaae3d8d37e31d8e56ffd2e1fca4373deaf
lv777 <q404023244@gmail.com>
2022-11-14 23:15:03 +0800

    🔥 删除前端 auth 项目

--- apps/auth/src/app/remote-entry/entry.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'fst-auth-entry',
-  template: `<fst-auth-nx-welcome></fst-auth-nx-welcome>`,
-})
-export class RemoteEntryComponent {}
