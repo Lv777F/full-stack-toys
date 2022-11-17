d44ae18e568d57e26e8fcc832d146ec5bdbefbfa
lv777 <q404023244@gmail.com>
2022-11-17 14:45:45 +0800

    ✨ 刷新 token

--- apps/api/src/user/user.controller.ts
+++ apps/api/src/user/user.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { GetUser } from './get-user.decorator';
+import { UserService } from './user.service';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  constructor(private userService: UserService) {}
+
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) userId: User['id']) {
+    return this.userService.getUserById(userId);
   }
 }
