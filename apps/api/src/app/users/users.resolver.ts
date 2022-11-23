13e352e94f51b1d1a6fe677de7da447e01bca035
lv777 <q404023244@gmail.com>
2022-11-23 17:01:43 +0800

    🔥 删除废弃逻辑

--- apps/api/src/app/users/users.resolver.ts
+++ apps/api/src/app/users/users.resolver.ts
@@ -1,34 +1,24 @@
-import { PaginatedPodcast, User } from '@full-stack-toys/dto';
+import { User } from '@full-stack-toys/dto';
 import { UseGuards } from '@nestjs/common';
-import {
-  Args,
-  Int,
-  Parent,
-  Query,
-  ResolveField,
-  Resolver,
-} from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { JwtAuthGuard } from '../auth/guard';
 import { CurrentUserId } from './get-user.decorator';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
   @UseGuards(JwtAuthGuard)
   @Query(() => User, { description: '当前账号的用户信息' })
   me(@CurrentUserId() userId: User['id']) {
-    return this.userService.findOne(userId);
+    return this.usersService.findOne(userId);
   }
 
   @Query(() => User, { description: '指定 id 的用户信息' })
-  user(@Args('id', { type: () => Int }) userId: User['id']) {
-    return this.userService.findOne(userId);
-  }
-
-  @ResolveField(() => PaginatedPodcast, { description: '用户关联的播客' })
-  podcasts(@Parent() user: User) {
-    console.log(user);
+  user(
+    @Args('id', { type: () => Int, description: '用户' }) userId: User['id']
+  ) {
+    return this.usersService.findOne(userId);
   }
 }
