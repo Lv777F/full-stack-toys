6e80f86e2c5b02be8cef8b14f65bde421a6549d3
lv777 <q404023244@gmail.com>
2022-11-28 23:48:35 +0800

    🛂 基础 Casl 配置

--- apps/api/src/app/casl/casl-ability.factory.ts
+++ apps/api/src/app/casl/casl-ability.factory.ts
@@ -1,4 +1,43 @@
+import { AbilityBuilder, PureAbility } from '@casl/ability';
+import { createPrismaAbility, PrismaQuery, Subjects } from '@casl/prisma';
 import { Injectable } from '@nestjs/common';
+import { Podcast, Post, Tag, User } from '@prisma/client';
 
+export enum Action {
+  Manage = 'manage',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+export type AppAbility = PureAbility<
+  [
+    Action,
+    Subjects<{ User: User; Podcast: Podcast; Post: Post; Tag: Tag }> | 'all'
+  ],
+  PrismaQuery
+>;
 @Injectable()
-export class CaslAbilityFactory {}
+export class CaslAbilityFactory {
+  createAbility(user?: Pick<User, 'id' | 'roles'>) {
+    const { can, cannot, build } = new AbilityBuilder<AppAbility>(
+      createPrismaAbility
+    );
+
+    can(Action.Read, 'Podcast', { published: true });
+    can(Action.Read, 'Post', { published: true });
+    can(Action.Read, 'User');
+
+    if (user) {
+      // TODO 添加 roles 到 jwt
+      can(Action.Update, 'User', { id: user.id });
+
+      can([Action.Read, Action.Update], 'Podcast', {
+        authors: { some: { authorId: { equals: user.id } } },
+      });
+    }
+
+    return build();
+  }
+}
