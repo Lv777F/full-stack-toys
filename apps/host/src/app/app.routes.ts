b06bc9ad29ae30fab8d641ffd7b3d580396c17e2
lv777 <q404023244@gmail.com>
2023-05-09 22:52:31 +0800

    🔥 remove host workspace

--- apps/host/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
-export const appRoutes: Route[] = [
-  {
-    path: '',
-    component: NxWelcomeComponent,
-  },
-];
